104224d46349dd1ddec4316040adacba0f0c0bad
Shalini Nayan <117961613+ShaliniNayan@users.noreply.github.com>
2023-04-12 19:38:09 +0530

    Add new branch and create folder name script add card details for Mobile project

---
+++ script/data.js
@@ -0,0 +1,71 @@
+// Mobile Version//
+const mobileProjects = [
+  {
+    //card 1 details
+    id: "1",
+    cardTitle: "TONIC",
+    description:
+      "A daily selection of privately personalized reads; no accounts or sign-ups required.",
+    company: "CANOPY",
+    cardTitle2: "TONIC",
+    description2:
+      "A daily selection of privately personalized; no accounts or sign-ups required.",
+    company2: "CONOPY",
+    no: "detail1",
+    cardImage1: "./images.Mobpics1.png",
+    counterText: "Back End Dev",
+    year: "2015",
+    technologies: ["HTML", "CSS", "JavaScript"],
+  },
+
+  {
+    id: "2",
+    cardTitle: "Multi-Post Stories",
+    description:
+      "A daily selection of privately personalized reads; no accounts or sign-ups required.",
+    company: "CANOPY",
+    cardTitle2: "TONIC",
+    description2:
+      "A daily selection of privately personalized; no accounts or sign-ups required.",
+    company2: "CONOPY",
+    no: "detail1",
+    cardImage1: "./images.Mobpics2.png",
+    counterText: "Back End Dev",
+    year: "2015",
+    technologies: ["HTML", "CSS", "JavaScript"],
+  },
+
+  {
+    id: "3",
+    cardTitle: "TONIC",
+    description:
+      "A daily selection of privately personalized reads; no accounts or sign-ups required.",
+    company: "CANOPY",
+    cardTitle2: "TONIC",
+    description2:
+      "A daily selection of privately personalized; no accounts or sign-ups required.",
+    company2: "CONOPY",
+    no: "detail1",
+    cardImage1: "./images.Mobpics3.png",
+    counterText: "Back End Dev",
+    year: "2015",
+    technologies: ["HTML", "CSS", "JavaScript"],
+  },
+
+  {
+    id: "4",
+    cardTitle: "Multi-Post Stories",
+    description:
+      "A daily selection of privately personalized reads; no accounts or sign-ups required.",
+    company: "CANOPY",
+    cardTitle2: "TONIC",
+    description2:
+      "A daily selection of privately personalized; no accounts or sign-ups required.",
+    company2: "CONOPY",
+    no: "detail1",
+    cardImage1: "./images.Mobpics4.png",
+    counterText: "Back End Dev",
+    year: "2015",
+    technologies: ["HTML", "CSS", "JavaScript"],
+  },
+];
